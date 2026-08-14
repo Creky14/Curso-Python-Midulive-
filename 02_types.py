de36ddf54259da29d44a4baa8bd1904d6677a2e8
Creky14 <ejose1806@gmail.com>
2026-08-14 14:52:45 -0400

    Primera clase 14/8/26

---
+++ 02_types.py
@@ -0,0 +1,38 @@
+###
+# 02 - types()
+# Python tiene varios tipos de datos
+# int, float, complex, str, bool, NoneType, list, tuple, dict, range, set...
+###
+
+print("int:")
+# print(type(10))
+# print(type(0))
+# print(type(-5))
+# print(type(123123123123123123123))
+# print(3908123098123098123098123123123123 + 1)
+
+print("float:")
+# print(type(3.14))
+# print(type(1.0))
+# print(type(1e3))
+
+print("complex:")
+# print(type(1 + 2j))
+
+print("str:")
+# print(type("Hola"))
+# print(type(""))
+# print(type("123"))
+# print(type(''))
+# print(type("""
+#   Multiliena
+#     """))
+
+print("bool:")
+# print(type(True)) 
+# print(type(False))
+# print(type(1 > 0))
+# print(type(1 < 2))
+
+print("NoneType:")
+# print(type(None))
---
+++ 03_cast.py
@@ -0,0 +1,28 @@
+###
+# 03 - casting de types
+# Transformar un tipo de un valor a otro (ejm: de un str a un num)
+###
+
+print("Conversión de tipos")
+
+# print(type(int("100")))
+
+# print(2 + int("100"))
+# print("100" + str(2)) "Resultado: 1002"
+
+# print(int(2.5))  Resultado: 2
+# print(round(2.5)) Resultado: 2
+# print(round(3.5)) Resultado: 4 (Redondeo al número par más cercano)
+
+# print(type(float("3.1416")))
+# print(int(3.1416))
+
+# print(bool(3))
+# print(bool(0))
+# print(bool(-1))
+
+# print(bool(""))
+# print(bool(" "))
+# print(bool("False"))
+
+# print(int("Hola mundo")) no es posible convertir un string que no sea un número a int
